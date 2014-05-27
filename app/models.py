a555737e2d594a67078a15be9d5eb3c8524d0698
timzdevz <timzdevz@gmail.com>
2014-05-27 21:44:07 +0300

    Add password hash to Monkey model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Monkey(db.Model):
     __tablename__ = 'monkeys'
@@ -6,7 +7,19 @@ class Monkey(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     email = db.Column(db.String(64), unique=True)
-    age = db.Column(db.Date())
+    password_hash = db.Column(db.String(128))
+    birth_date = db.Column(db.Date())
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute') 
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {} {}>'.format(self.id, self.email)
