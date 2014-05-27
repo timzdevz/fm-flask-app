40bdc40485a8a62e8b438df1a10b6aa9090acc0e
timzdevz <timzdevz@gmail.com>
2014-05-28 00:43:15 +0300

    Add registration handling functionality

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, DateField, SubmitField
-from wtforms.validators import Required, Email, Length
+from wtforms import StringField, PasswordField, DateField, SubmitField, \
+                    ValidationError
+from wtforms.validators import Required, Email, Length, EqualTo
+from .models import Monkey
 
 class LoginForm(Form):
     email = StringField('Your email', validators=[Email(), Required()])
@@ -11,8 +13,14 @@ class RegistrationForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     email = StringField('Your email', \
         validators=[Email(), Required()])
-    password = PasswordField('Your password', validators=[Length(min=6, \
-            message='Your password must at least 6 char. long')])
+    password = PasswordField('Your password', validators=[
+        Length(min=6, message='Password must be at least 6 charachter long')])
+    password2 = PasswordField('Confirm your password', validators=[
+        Required(), EqualTo('password', message='Passwords must match.')])
     birth_date = DateField('Your birth date',\
         description='Use DD/MM/YYYY format', format='%d/%m/%Y')
     submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if Monkey.query.filter_by(email=field.data).first():
+            raise ValidationError('Email has been already registered.')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from . import app
+from flask import render_template, flash, redirect, url_for
+from . import app, db
 from .forms import RegistrationForm, LoginForm
+from .models import Monkey
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,6 +13,18 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+
     if form.validate_on_submit():
-        pass
+        monkey = Monkey(
+            name=form.name.data,
+            email=form.email.data,
+            password=form.password.data,
+            birth_date=form.birth_date.data)
+
+        db.session.add(monkey)
+        db.session.commit()     
+
+        flash('You have successfully registered! You can login now', 'success')
+        return redirect(url_for('index'))
+
     return render_template('register.html', form=form)
