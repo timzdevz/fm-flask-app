d6c326ef7544eaabaffe3f92a7cc04d576e3d42b
timzdevz <timzdevz@gmail.com>
2014-05-29 10:29:19 +0300

    Export Monkey model to shell

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 from app import app, db
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+from app.models import Monkey
 
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Monkey=Monkey)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
