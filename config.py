81b6c698b2a40499c36a4d379a3bb9a29a61f191
timzdevz <timzdevz@gmail.com>
2014-06-04 08:52:54 +0300

    Change dev db to sqlite

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://admin:adminpass@localhost/fastmonkeys"
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class HerokuConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
